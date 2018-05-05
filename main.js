fe41e833d57c3f00dd8e05a868c55a688d7f85c9
Cody Romano <codyromano@gmail.com>
2018-05-05 11:42:55 -0700

    Dancing update

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ import {KNNImageClassifier} from 'deeplearn-knn-image-classifier';
 import * as dl from 'deeplearn';
 
 // Number of classes to classify
-const NUM_CLASSES = 3;
-// Webcam Image size. Must be 227. 
+const NUM_CLASSES = 2;
+// Webcam Image size. Must be 227.
 const IMAGE_SIZE = 227;
 // K value for KNN
 const TOPK = 10;
@@ -29,41 +29,73 @@ class Main {
     this.infoTexts = [];
     this.training = -1; // -1 when no class is being trained
     this.videoPlaying = false;
-    
+
     // Initiate deeplearn.js math and knn classifier objects
     this.knn = new KNNImageClassifier(NUM_CLASSES, TOPK);
-    
+
     // Create video element that will contain the webcam image
     this.video = document.createElement('video');
     this.video.setAttribute('autoplay', '');
     this.video.setAttribute('playsinline', '');
-    
+
+    this.music = document.querySelector('.music');
+
+    this.classMetadata = {
+      0: {
+        id: 'dancing',
+        label: 'Start Dancing',
+        undoLabel: 'Stop Dancing'
+      },
+      1: {
+        id: 'notDancing',
+        label: 'Start Walking Normally',
+        undoLabel: 'Stop Walking'
+      }
+    };
+
     // Add video element to DOM
     document.body.appendChild(this.video);
-    
-    // Create training buttons and info texts    
+
+    // Create training buttons and info texts
     for(let i=0;i<NUM_CLASSES; i++){
       const div = document.createElement('div');
       document.body.appendChild(div);
-      div.style.marginBottom = '10px';
+      // div.style.marginBottom = '10px';
 
       // Create training button
       const button = document.createElement('button')
-      button.innerText = "Train "+i;
+      const buttonText = this.classMetadata[i].label;
+      button.innerText = buttonText;
       div.appendChild(button);
 
       // Listen for mouse events when clicking the button
+      /*
       button.addEventListener('mousedown', () => this.training = i);
       button.addEventListener('mouseup', () => this.training = -1);
-      
+      */
+      button.addEventListener('click', () => {
+        const ACTIVE_CLASS = 'training';
+
+        if (button.classList.contains(ACTIVE_CLASS)) {
+          this.training = -1;
+          button.innerText = this.classMetadata[i].label;
+          button.classList.remove(ACTIVE_CLASS);
+
+        } else {
+          this.training = i;
+          button.innerText = this.classMetadata[i].undoLabel;
+          button.classList.add(ACTIVE_CLASS);
+        }
+      });
+
       // Create info text
       const infoText = document.createElement('span')
       infoText.innerText = " No examples added";
       div.appendChild(infoText);
       this.infoTexts.push(infoText);
     }
-    
-    
+
+
     // Setup webcam
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then((stream) => {
@@ -74,12 +106,12 @@ class Main {
       this.video.addEventListener('playing', ()=> this.videoPlaying = true);
       this.video.addEventListener('paused', ()=> this.videoPlaying = false);
     })
-    
+
     // Load knn model
     this.knn.load()
-    .then(() => this.start()); 
+    .then(() => this.start());
   }
-  
+
   start(){
     if (this.timer) {
       this.stop();
@@ -87,23 +119,23 @@ class Main {
     this.video.play();
     this.timer = requestAnimationFrame(this.animate.bind(this));
   }
-  
+
   stop(){
     this.video.pause();
     cancelAnimationFrame(this.timer);
   }
-  
+
   animate(){
     if(this.videoPlaying){
       // Get image data from video element
       const image = dl.fromPixels(this.video);
-      
+
       // Train class if one of the buttons is held down
       if(this.training != -1){
         // Add current image to classifier
         this.knn.addImage(image, this.training)
       }
-      
+
       // If any examples have been added, run predict
       const exampleCount = this.knn.getClassExampleCount();
       if(Math.max(...exampleCount) > 0){
@@ -112,6 +144,13 @@ class Main {
           for(let i=0;i<NUM_CLASSES; i++){
             // Make the predicted class bold
             if(res.classIndex == i){
+              if (res.confidences[i]*100 >= 80) {
+                if (this.classMetadata[i].id === 'dancing') {
+                  this.music.play();
+                } else {
+                  this.music.pause();
+                }
+              }
               this.infoTexts[i].style.fontWeight = 'bold';
             } else {
               this.infoTexts[i].style.fontWeight = 'normal';
@@ -119,7 +158,8 @@ class Main {
 
             // Update info text
             if(exampleCount[i] > 0){
-              this.infoTexts[i].innerText = ` ${exampleCount[i]} examples - ${res.confidences[i]*100}%`
+              this.infoTexts[i].innerText = ` ${exampleCount[i]} examples`;
+              // this.infoTexts[i].innerText = this.classMetadata[id].description;
             }
           }
         })
